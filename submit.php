861297e1662a0b3bce64321dcba21e7b24ea1c51
Balachandar s <47241817+Balachanderss@users.noreply.github.com>
2021-01-12 11:39:07 +0530

    Contact Submit

---
+++ submit.php
@@ -0,0 +1,15 @@
+<?php
+/* $con=mysqli_connect('localhost','root','','website');
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$message=$_REQUEST['message'];
+		$submit=mysqli_query($con,"insert into contact(name,email,message,created_on)values('$name','$email','$message',now())");
+	if($submit)
+	{
+		echo 1;
+	}
+	else
+	{
+		echo 0;
+	}
+ */	?>
